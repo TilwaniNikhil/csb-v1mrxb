96b0e222b06b8b63d86e6ead385a5982297e4d0b
Code Sandbox <hello@codesandbox.io>
2022-10-10 15:14:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b507460d1830c84e341e0193c2c62bd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614a05c0589832d4c432e5c6a151ec33",
+    "url": "https://tilwaninikhil.github.io/csb-v1mrxb/index.html"
+  },
+  {
+    "revision": "a5ad968d4f9ce0ae0e71",
+    "url": "https://tilwaninikhil.github.io/csb-v1mrxb/static/css/main.c41ee3b4.chunk.css"
+  },
+  {
+    "revision": "5859501d389f6ec2a27e",
+    "url": "https://tilwaninikhil.github.io/csb-v1mrxb/static/js/2.d500e239.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://tilwaninikhil.github.io/csb-v1mrxb/static/js/2.d500e239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ad968d4f9ce0ae0e71",
+    "url": "https://tilwaninikhil.github.io/csb-v1mrxb/static/js/main.2acf92c2.chunk.js"
+  },
+  {
+    "revision": "9c3b677f7d870333e02c",
+    "url": "https://tilwaninikhil.github.io/csb-v1mrxb/static/js/runtime-main.61449521.js"
+  }
+]);
